f4f2b9e10cea3db494c6642d447ea3a53068e962
Vladimir Pochtarenko <wooldik94@gmail.com>
2020-10-18 16:39:22 +0300

    added all const

---
+++ script.js
@@ -0,0 +1,6 @@
+const right = document.querySelector('.slider__control_right');
+const left = document.querySelector('.slider__control_left');
+const slides = document.getElementsByClassName('slider__item');
+
+
+
