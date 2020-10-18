7c075eae47e151066fe8f5effffe628e133b8ea4
Vladimir Pochtarenko <wooldik94@gmail.com>
2020-10-18 17:12:23 +0300

    added flipping right and left

--- script.js
+++ script.js
@@ -2,23 +2,31 @@ const right = document.querySelector('.slider__control_right');
 const left = document.querySelector('.slider__control_left');
 const slides = document.getElementsByClassName('slider__item');
 
+let slideIndex = 1;
+showSlides(slideIndex);
 
+right.addEventListener('click', () => {
+	showSlides(++slideIndex);
+})
+
+left.addEventListener('click', () => {
+	showSlides(--slideIndex);
+})
 
 
 function showSlides(n) {
 	if (n > slides.length) {
 		slideIndex = 1;
-
-	} else if (n < 1) {
-		slideIndex = slides.length
-
+	}
+	if (n === 0) {
+		slideIndex = slides.length;
 	}
 
 	for (let slide of slides) {
-		console.log(slide);
 		slide.style.display = "none";
 	}
 	slides[slideIndex - 1].style.display = "block";
-	console.log(slideIndex);
+
+
 }
 
