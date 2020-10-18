6d55d6febcd4e4abd7b1accce29edc1b22026046
Vladimir Pochtarenko <wooldik94@gmail.com>
2020-10-18 16:40:13 +0300

    flipping

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ const slides = document.getElementsByClassName('slider__item');
 
 
 
+
+function showSlides(n) {
+	if (n > slides.length) {
+		slideIndex = 1;
+
+	} else if (n < 1) {
+		slideIndex = slides.length
+
+	}
+
+	for (let slide of slides) {
+		console.log(slide);
+		slide.style.display = "none";
+	}
+	slides[slideIndex - 1].style.display = "block";
+	console.log(slideIndex);
+}
+
